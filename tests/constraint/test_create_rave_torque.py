53030a2d9c162f05a28795dd0c6f058e3eab86ad
Hung Pham <hungpham2511@gmail.com>
2018-05-12 21:15:26 +0800

    add test

---
+++ tests/constraint/test_create_rave_torque.py
@@ -0,0 +1,22 @@
+import pytest
+import toppra
+import numpy as np
+
+
+@pytest.fixture(scope="module")
+def barret_robot(rave_env):
+    rave_env.Reset()
+    rave_env.Load("data/lab1.env.xml")
+    robot = rave_env.GetRobots()[0]
+    yield robot
+
+
+@pytest.mark.parametrize("dof", [3, 5, 7])
+def test_torque_bound_barret(barret_robot, dof):
+    barret_robot.SetActiveDOFs(range(dof))
+    constraint = toppra.create_rave_torque_path_constraint(barret_robot)
+    np.random.seed(0)
+    path = toppra.SplineInterpolator(np.linspace(0, 1, 5), np.random.rand(5, dof))
+    a, b, c, F, g, _, _ = constraint.compute_constraint_params(path, np.linspace(0, path.get_duration(), 5))
+
+    
