59ac99a34f7db1eaca332af7d1baff64853c1fe3
Hung Pham <hungpham2511@gmail.com>
2018-05-14 11:50:32 +0800

    edit kinematics example

--- examples/kinematics.py
+++ examples/kinematics.py
@@ -3,50 +3,56 @@ import toppra.constraint as constraint
 import toppra.algorithm as algo
 import numpy as np
 import matplotlib.pyplot as plt
-import coloredlogs
-coloredlogs.install(level='DEBUG')
+import argparse
 
-# Parameters
-N = 300
-N_samples = 5
-SEED = 9
-dof = 7
 
-# Random waypoints used to obtain a random geometric path.
-np.random.seed(SEED)
-way_pts = np.random.randn(N_samples, dof)
+def main():
+    # Parameters
+    N = 300
+    N_samples = 5
+    SEED = 9
+    dof = 7
 
-# Create velocity bounds, then velocity constraint object
-vlim_ = np.random.rand(dof) * 20
-vlim = np.vstack((-vlim_, vlim_)).T
-# Create acceleration bounds, then acceleration constraint object
-alim_ = np.random.rand(dof) * 2
-alim = np.vstack((-alim_, alim_)).T
+    # Random waypoints used to obtain a random geometric path.
+    np.random.seed(SEED)
+    way_pts = np.random.randn(N_samples, dof)
 
-path = ta.SplineInterpolator(np.linspace(0, 1, 5), way_pts)
-pc_vel = constraint.JointVelocityConstraint(vlim)
-pc_acc = constraint.JointAccelerationConstraint(
-    alim, discretization_scheme=constraint.DiscretizationType.Interpolation)
-instance = algo.TOPPRA([pc_vel, pc_acc], path, gridpoints=np.linspace(0, 1, 101), solver_wrapper='qpOASES')
+    # Create velocity bounds, then velocity constraint object
+    vlim_ = np.random.rand(dof) * 20
+    vlim = np.vstack((-vlim_, vlim_)).T
+    # Create acceleration bounds, then acceleration constraint object
+    alim_ = np.random.rand(dof) * 2
+    alim = np.vstack((-alim_, alim_)).T
 
-X = instance.compute_feasible_sets()
-K = instance.compute_controllable_sets(0, 0)
+    path = ta.SplineInterpolator(np.linspace(0, 1, 5), way_pts)
+    pc_vel = constraint.JointVelocityConstraint(vlim)
+    pc_acc = constraint.JointAccelerationConstraint(
+        alim, discretization_scheme=constraint.DiscretizationType.Interpolation)
+    instance = algo.TOPPRA([pc_vel, pc_acc], path, gridpoints=np.linspace(0, 1, 101))
 
-_, sd_vec, _ = instance.compute_parameterization(0, 0)
+    X = instance.compute_feasible_sets()
+    K = instance.compute_controllable_sets(0, 0)
 
-X = np.sqrt(X)
-K = np.sqrt(K)
+    _, sd_vec, _ = instance.compute_parameterization(0, 0)
 
-plt.plot(X[:, 0], c='green')
-plt.plot(X[:, 1], c='green')
-plt.plot(K[:, 0], '--', c='red')
-plt.plot(K[:, 1], '--', c='red')
-plt.plot(sd_vec)
-plt.show()
+    X = np.sqrt(X)
+    K = np.sqrt(K)
 
-jnt_traj, aux_traj = instance.compute_trajectory(0, 0)
-ts_sample = np.linspace(0, jnt_traj.get_duration(), 100)
-qs_sample = jnt_traj.evaldd(ts_sample)
+    plt.plot(X[:, 0], c='green', label="Feasible sets")
+    plt.plot(X[:, 1], c='green')
+    plt.plot(K[:, 0], '--', c='red', label="Controllable sets")
+    plt.plot(K[:, 1], '--', c='red')
+    plt.plot(sd_vec, label="Velocity profile")
+    plt.legend()
+    plt.title("Path-position path-velocity plot")
+    plt.show()
 
-plt.plot(ts_sample, qs_sample)
-plt.show()
+    jnt_traj, aux_traj = instance.compute_trajectory(0, 0)
+    ts_sample = np.linspace(0, jnt_traj.get_duration(), 100)
+    qs_sample = jnt_traj.evaldd(ts_sample)
+
+    plt.plot(ts_sample, qs_sample)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
