99c2bbb4d81a58686d0c515c9b31fab9eb283885
Hung Pham <hungpham2511@gmail.com>
2019-02-06 13:45:00 +0800

    improve robustness reporting with pandas

--- tests/retime_robustness/test_robustness_main.py
+++ tests/retime_robustness/test_robustness_main.py
@@ -1,24 +1,23 @@
 import pytest
 import numpy as np
-import numpy.testing as npt
 import yaml
 import re
+import pandas
 
 import toppra
 import toppra.constraint as constraint
 import toppra.algorithm as algo
 
+toppra.setup_logging(level="FATAL")
 
-toppra.setup_logging(level="INFO")
-
-problem_regex = "prob1.*oa.*"
+# NOTE: Select problems to test with this regex.
+problem_regex = ".*oa.*"
 
 
 def test_robustness_main():
-    """ Load problem suite and test all.
+    """ Load problem suite based on regex, run test and report results.
     """
-
-    # parse all problems from configuration file
+    # parse problems from a configuration file
     parsed_problems = []
     with open("retime_robustness/problem_suite_1.yaml", "r") as f:
         problem_dict = yaml.load(f.read())
@@ -31,45 +30,48 @@ def test_robustness_main():
         for duration in problem_dict[key]['desired_duration']:
             for solver_wrapper in problem_dict[key]['solver_wrapper']:
                 for nb_gridpoints in problem_dict[key]['nb_gridpoints']:
-                    parsed_problems.append([
-                        "{:}-{:5f}-{:}-{:}".format(key, duration, solver_wrapper, nb_gridpoints),
-                        np.array(problem_dict[key]['waypoints'], dtype=float),
-                        ss_waypoints,
-                        np.r_[problem_dict[key]['vlim']],
-                        np.r_[problem_dict[key]['alim']],
-                        duration,
-                        solver_wrapper,
-                        np.linspace(ss_waypoints[0], ss_waypoints[-1], nb_gridpoints)
-                    ])
+                    parsed_problems.append({
+                        "name": key,
+                        "problem_id": "{:}-{:5f}-{:}-{:}".format(key, duration, solver_wrapper, nb_gridpoints),
+                        'waypoints': np.array(problem_dict[key]['waypoints'], dtype=float),
+                        'ss_waypoints': ss_waypoints,
+                        'vlim': np.r_[problem_dict[key]['vlim']],
+                        'alim': np.r_[problem_dict[key]['alim']],
+                        'desired_duration': duration,
+                        'solver_wrapper': solver_wrapper,
+                        'gridpoints': np.linspace(ss_waypoints[0], ss_waypoints[-1], nb_gridpoints),
+                        'nb_gridpoints': nb_gridpoints
+                    })
+    parsed_problems_df = pandas.DataFrame(parsed_problems)
 
-    # attempt to solve
+    # solve problems that matched the given regex
     all_success = True
-    all_res = []
-    for problem_data in parsed_problems:
-        if re.match(problem_regex, problem_data[0]) is None:
+    for row_index, problem_data in parsed_problems_df.iterrows():
+        if re.match(problem_regex, problem_data['problem_id']) is None:
             continue
-        path = toppra.SplineInterpolator(problem_data[2], problem_data[1])
-        vlim = np.vstack((- problem_data[3], problem_data[3])).T
-        alim = np.vstack((- problem_data[3], problem_data[3])).T
+        path = toppra.SplineInterpolator(
+            problem_data['ss_waypoints'],
+            problem_data['waypoints'])
+        vlim = np.vstack((- problem_data['vlim'], problem_data['vlim'])).T
+        alim = np.vstack((- problem_data['alim'], problem_data['alim'])).T
         pc_vel = constraint.JointVelocityConstraint(vlim)
         pc_acc = constraint.JointAccelerationConstraint(
             alim, discretization_scheme=constraint.DiscretizationType.Interpolation)
 
-        instance = algo.TOPPRA([pc_vel, pc_acc], path, gridpoints=problem_data[7],
-                               solver_wrapper=problem_data[6])
-
+        instance = algo.TOPPRA([pc_vel, pc_acc], path, gridpoints=problem_data['gridpoints'],
+                               solver_wrapper=problem_data['solver_wrapper'])
         jnt_traj, aux_traj, data = instance.compute_trajectory(0, 0, return_data=True)
 
         if jnt_traj is None:
             all_success = False
-            res_string = "FAIL             {:}".format(problem_data[0])
+            parsed_problems_df.loc[row_index, "status"] = "FAIL"
         else:
-            res_string = "SUCCESS {:5f} {:}".format(jnt_traj.get_duration(), problem_data[0])
-        all_res.append(res_string)
+            parsed_problems_df.loc[row_index, "status"] = "SUCCESS"
+            parsed_problems_df.loc[row_index,
+                                   "duration"] = jnt_traj.get_duration()
 
-    print("\nTest results:")
-    for res in all_res:
-        print(res)
+    # get all rows with status different from NaN, then reports other columns.
+    result_df = parsed_problems_df[parsed_problems_df["status"].notna()][
+        ["status", "duration", "name", "solver_wrapper", "nb_gridpoints"]]
+    print(result_df)
     assert all_success, "Unable to solve some problems in the test suite"
-        
-
