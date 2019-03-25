6c05fabfca2a2c4fadf74b7184e3ede25c965fab
lin <287328906@qq.com>
2019-03-25 19:36:49 +0800

    add dry friction in toppra

--- toppra/constraint/__init__.py
+++ toppra/constraint/__init__.py
@@ -1,6 +1,11 @@
-from .constraint import ConstraintType, DiscretizationType
+""" Module
+"""
+from .constraint import ConstraintType, DiscretizationType, Constraint
 from .joint_acceleration import JointAccelerationConstraint
 from .joint_velocity import JointVelocityConstraint, JointVelocityConstraintVarying
 from .can_linear_second_order import CanonicalLinearSecondOrderConstraint, canlinear_colloc_to_interpolate
 from .canonical_conic import RobustCanonicalLinearConstraint
 from .canonical_linear import CanonicalLinearConstraint
+from .joint_torque import JointTorqueConstraint
+
+
