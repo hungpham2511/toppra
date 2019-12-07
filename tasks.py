c22323703178611e58517134779f11efd19e6ecf
Hung <hungpham2511@gmail.com>
2019-12-07 23:36:26 +0800

    add docker tasks

--- tasks.py
+++ tasks.py
@@ -16,3 +16,24 @@ def install_solvers(c):
         c.run("git clone https://github.com/hungpham2511/qpOASES {}".format(install_dir))
         c.run("cd /tmp/tox-qpoases/ && mkdir bin && make")
     c.run("cd /tmp/tox-qpoases/interfaces/python/ && python setup.py install")
+
+
+@task
+def docker_build(c):
+    c.run("docker build -f dockerfiles/Dockerfile . -t toppra-dev")
+
+
+@task
+def docker_start(c):
+    c.run("docker run --rm --name toppra-dep -d \
+    -v /home/hung/git/toppra:$HOME/toppra \
+    hungpham2511/toppra-dep:0.0.2 sleep infinity")
+
+
+@task
+def docker_exec(c):
+    c.run("docker exec -it toppra-dep bash", pty=True)
+
+@task
+def docker_stop(c):
+    c.run("docker stop toppra-dep")
