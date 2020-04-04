c1d484b7022847c55ca9de349fc5736658c6c0ec
Hung <hungpham2511@gmail.com>
2020-04-04 16:07:44 +0800

    Finish simple path for scalar traj

--- tests/interpolators/test_simple_path.py
+++ tests/interpolators/test_simple_path.py
@@ -29,3 +29,14 @@ def test_first_derivative_zero_at_endpoints(f_scalar):
     assert_allclose(f_scalar(0, 1), np.array(0))
     assert_allclose(f_scalar(2, 1), np.array(0))
 
+
+@pytest.fixture(name="f_scalar_wd")
+def given_a_simple_scalar_function_with_first_derivatives():
+    f = toppra.SimplePath([0, 1, 2], [0, 1, 1], [0, 2, 0])
+    yield f
+
+
+def test_correct_derivatives_as_specified(f_scalar_wd):
+    assert_allclose(f_scalar_wd(1, 1), 2.0)
+    assert_allclose(f_scalar_wd(0, 1), 0)
+    assert_allclose(f_scalar_wd(2, 1), 0)
--- toppra/simplepath.py
+++ toppra/simplepath.py
@@ -1,18 +1,17 @@
-from typing import List, Union
+from typing import List, Union, Optional
 import numpy as np
 from scipy.interpolate import BPoly
 
 
 class SimplePath:
-
-    def __init__(self, x: List[float], y: Union[List[float], np.ndarray], yd=None, ydd=None):
+    def __init__(self, x: np.ndarray, y: np.ndarray, yd: np.ndarray = None, ydd=None):
         if yd is None:
             yd = np.zeros_like(x, dtype=float)
             for i in range(1, len(y) - 1):
                 yd[i] = (y[i + 1] - y[i - 1]) / (x[i + 1] - x[i - 1])
-        y = np.vstack((y, yd)).T
+        y_with_derivatives = np.vstack((y, yd)).T
 
-        self._poly = BPoly.from_derivatives(x, y)
+        self._poly = BPoly.from_derivatives(x, y_with_derivatives)
 
     def __call__(self, xi, order=0):
         if order == 1:
