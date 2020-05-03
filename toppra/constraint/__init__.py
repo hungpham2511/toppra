45de4afa62f204b61e93d028986f7fd60938a778
Hung <hungpham2511@gmail.com>
2020-05-03 12:32:37 +0800

    [docs] Improve the documentation for constraint in Python code

--- toppra/constraint/__init__.py
+++ toppra/constraint/__init__.py
@@ -1,4 +1,61 @@
-"""This module defines different dynamic constraints."""
+"""Modules implementing different dynamics constraints.
+
+Base abstractions and enum
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autoclass:: toppra.constraint.Constraint
+   :members: compute_constraint_params, set_discretization_type, __repr__
+
+.. autoclass:: toppra.constraint.ConstraintType
+   :members:
+
+.. autoclass:: toppra.constraint.LinearConstraint
+   :members:
+   :special-members:
+   :show-inheritance:
+
+.. autoclass:: toppra.constraint.DiscretizationType
+   :members:
+
+Velocity Constraints
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autoclass:: toppra.constraint.JointVelocityConstraint
+   :members:
+   :special-members:
+   :show-inheritance:
+
+.. autoclass:: toppra.constraint.JointVelocityConstraintVarying
+   :members:
+   :special-members:
+   :show-inheritance:
+
+Second Order Constraints
+^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autoclass:: toppra.constraint.SecondOrderConstraint
+   :members:
+   :special-members:
+   :show-inheritance:
+
+.. autoclass:: toppra.constraint.JointTorqueConstraint
+   :members:
+   :special-members:
+   :show-inheritance:
+
+.. autoclass:: toppra.constraint.JointAccelerationConstraint
+   :members:
+   :special-members:
+   :show-inheritance:
+
+Robust Linear Constraint
+^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. autoclass:: toppra.constraint.RobustLinearConstraint
+   :members:
+   :show-inheritance:
+
+Misc
+^^^^^^^^^^
+.. autofunction:: toppra.constraint.canlinear_colloc_to_interpolate
+
+"""
 from .constraint import ConstraintType, DiscretizationType, Constraint
 from .joint_torque import JointTorqueConstraint
 from .linear_joint_acceleration import JointAccelerationConstraint
