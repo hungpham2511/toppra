e702db8f7484cc678ca04579e7d892c438b7e032
Hung <hungpham2511@gmail.com>
2020-05-17 11:43:39 +0800

    Initial bindings

---
+++ cpp/bindings/toppra_int.cpp
@@ -0,0 +1,20 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+
+#include <toppra/toppra.hpp>
+#include <toppra/geometric_path.hpp>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(toppra_int, m) {
+  m.doc() = "pybind11 example plugin"; // optional module docstring
+  m.def("add", &add, "A function which adds two numbers");
+  py::class_<toppra::PiecewisePolyPath>(m, "PiecewisePolyPath")
+      .def(py::init<>())
+      .def(py::init<const toppra::Matrices&, std::vector<toppra::value_type>>())
+      .def("eval_single", &toppra::PiecewisePolyPath::eval_single)
+      .def("eval", &toppra::PiecewisePolyPath::eval)
+      .def("pathInterval", &toppra::PiecewisePolyPath::pathInterval);
+}
+
