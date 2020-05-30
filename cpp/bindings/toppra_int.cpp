4b15f2f450eaed5c0a7e701fcf0361177d41d02f
Joseph Mirabel <jmirabel@laas.fr>
2020-05-30 16:23:54 +0200

    [cpp] move bindings of geometric paths into separate file.

--- cpp/bindings/toppra_int.cpp
+++ cpp/bindings/toppra_int.cpp
@@ -16,52 +16,13 @@ namespace py = pybind11;
 namespace toppra {
 namespace python {
 
-nparr path_eval (const PiecewisePolyPath& p, const Vector& xs, int order=0)
-{
-  return toNumpyArray(p.eval(xs,order));
-}
-
-template<int order>
-nparr path_eval_tpl (const PiecewisePolyPath& p, const Vector& xs)
-{
-  return toNumpyArray(p.eval(xs,order));
-}
-
+void exposePaths(py::module m);
 void exposeConstraints(py::module m);
 
 PYBIND11_MODULE(toppra_int, m) {
   m.doc() = "toppra C++ bindings (internal)";
 
-  py::class_<PiecewisePolyPath, std::shared_ptr<PiecewisePolyPath> >(m, "PiecewisePolyPath")
-      .def(py::init<>())
-      .def(py::init<const toppra::Matrices&, std::vector<toppra::value_type>>())
-      .def("eval_single", &PiecewisePolyPath::eval_single)
-      .def("eval", &path_eval, py::arg("xs"), py::arg("order") = 0)
-      .def("evald", &path_eval_tpl<1>)
-      .def("evald", &path_eval_tpl<2>)
-      .def("serialize", [](const PiecewisePolyPath& p) -> py::bytes {
-            std::ostringstream ss;
-            p.serialize(ss);
-            return ss.str();
-          })
-      .def("deserialize", [](PiecewisePolyPath& p, const py::bytes& b) {
-            std::stringstream ss;
-            ss << b.cast<std::string>();
-            p.deserialize(ss);
-          })
-      .def("__call__", &path_eval, py::arg("xs"), py::arg("order") = 0)
-      .def("__call__", &PiecewisePolyPath::eval_single, py::arg("x"), py::arg("order") = 0)
-      .def("__str__", [](const PiecewisePolyPath& p) -> std::string { return "PiecewisePolyPath(...)"; })
-      .def("__repr__", [](const PiecewisePolyPath& p) -> std::string { return "PiecewisePolyPath(...)"; })
-      .def_property_readonly("dof", &PiecewisePolyPath::dof)
-      .def_property_readonly("path_interval", &PiecewisePolyPath::pathInterval)
-      .def_property_readonly("duration",
-          [] (const PiecewisePolyPath& p) -> double {
-            Bound bd (p.pathInterval());
-            return bd[1] - bd[0];
-          })
-      .def_static("constructHermite", &PiecewisePolyPath::constructHermite);
-
+  exposePaths(m);
   exposeConstraints(m);
 
   // algorithm
@@ -84,7 +45,7 @@ PYBIND11_MODULE(toppra_int, m) {
       .def_readwrite("ret_code", &toppra::ParametrizationData::ret_code);
 
   py::class_<algorithm::TOPPRA>(m, "TOPPRA")
-      .def(py::init<LinearConstraintPtrs, const std::shared_ptr<PiecewisePolyPath> &>())
+      .def(py::init<LinearConstraintPtrs, const GeometricPathPtr &>())
       .def("computePathParametrization", &algorithm::TOPPRA::computePathParametrization,
            py::arg("vel_start") = 0, py::arg("vel_end") = 0)
       .def("setN", &algorithm::TOPPRA::setN)
