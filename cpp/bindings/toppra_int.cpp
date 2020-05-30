7e81ecdbf7ca8d721d2a0646216131fa93fb191d
Joseph Mirabel <jmirabel@laas.fr>
2020-05-30 15:55:48 +0200

    [cpp] Python bindings directly use the C++ interface.

--- cpp/bindings/toppra_int.cpp
+++ cpp/bindings/toppra_int.cpp
@@ -33,6 +33,17 @@ toppra::constraint::jointTorque::Pinocchio<> createJointTorque(py::object model,
 namespace toppra {
 namespace python {
 
+nparr path_eval (const PiecewisePolyPath& p, const Vector& xs, int order=0)
+{
+  return toNumpyArray(p.eval(xs,order));
+}
+
+template<int order>
+nparr path_eval_tpl (const PiecewisePolyPath& p, const Vector& xs)
+{
+  return toNumpyArray(p.eval(xs,order));
+}
+
 PYBIND11_MODULE(toppra_int, m) {
   m.doc() = "toppra C++ bindings (internal)";
   py::enum_<toppra::DiscretizationType>(m, "DiscretizationType")
@@ -40,23 +51,35 @@ PYBIND11_MODULE(toppra_int, m) {
       .value("Interpolation", toppra::DiscretizationType::Interpolation)
       .export_values();
 
-  py::class_<PyPiecewisePolyPath>(m, "PiecewisePolyPath")
+  py::class_<PiecewisePolyPath, std::shared_ptr<PiecewisePolyPath> >(m, "PiecewisePolyPath")
       .def(py::init<>())
       .def(py::init<const toppra::Matrices&, std::vector<toppra::value_type>>())
-      .def("eval_single", &PyPiecewisePolyPath::eval_single)
-      .def("eval", &PyPiecewisePolyPath::eval, py::arg("xs"), py::arg("order") = 0)
-      .def("evald", &PyPiecewisePolyPath::evald, py::arg("xs"))
-      .def("evaldd", &PyPiecewisePolyPath::evaldd, py::arg("xs"))
-      .def("serialize", &PyPiecewisePolyPath::serialize)
-      .def("deserialize", &PyPiecewisePolyPath::deserialize)
-      .def("__call__", &PyPiecewisePolyPath::eval, py::arg("xs"), py::arg("order") = 0)
-      .def("__call__", &PyPiecewisePolyPath::eval_single, py::arg("x"), py::arg("order") = 0)
-      .def("__str__", &PyPiecewisePolyPath::__str__)
-      .def("__repr__", &PyPiecewisePolyPath::__repr__)
-      .def_property_readonly("dof", &PyPiecewisePolyPath::dof)
-      .def_property_readonly("path_interval", &PyPiecewisePolyPath::pathInterval)
-      .def_property_readonly("duration", &PyPiecewisePolyPath::duration)
-      .def_static("constructHermite", &PyPiecewisePolyPath::constructHermite);
+      .def("eval_single", &PiecewisePolyPath::eval_single)
+      .def("eval", &path_eval, py::arg("xs"), py::arg("order") = 0)
+      .def("evald", &path_eval_tpl<1>)
+      .def("evald", &path_eval_tpl<2>)
+      .def("serialize", [](const PiecewisePolyPath& p) -> py::bytes {
+            std::ostringstream ss;
+            p.serialize(ss);
+            return ss.str();
+          })
+      .def("deserialize", [](PiecewisePolyPath& p, const py::bytes& b) {
+            std::stringstream ss;
+            ss << b.cast<std::string>();
+            p.deserialize(ss);
+          })
+      .def("__call__", &path_eval, py::arg("xs"), py::arg("order") = 0)
+      .def("__call__", &PiecewisePolyPath::eval_single, py::arg("x"), py::arg("order") = 0)
+      .def("__str__", [](const PiecewisePolyPath& p) -> std::string { return "PiecewisePolyPath(...)"; })
+      .def("__repr__", [](const PiecewisePolyPath& p) -> std::string { return "PiecewisePolyPath(...)"; })
+      .def_property_readonly("dof", &PiecewisePolyPath::dof)
+      .def_property_readonly("path_interval", &PiecewisePolyPath::pathInterval)
+      .def_property_readonly("duration",
+          [] (const PiecewisePolyPath& p) -> double {
+            Bound bd (p.pathInterval());
+            return bd[1] - bd[0];
+          })
+      .def_static("constructHermite", &PiecewisePolyPath::constructHermite);
 
   // Abstract class must be binded for derived classes to work
   py::class_<LinearConstraint, LinearConstraintPtr>(m, "_LinearConstraint")
@@ -122,13 +145,12 @@ PYBIND11_MODULE(toppra_int, m) {
       .def_readwrite("feasible_sets", &toppra::ParametrizationData::feasible_sets)
       .def_readwrite("ret_code", &toppra::ParametrizationData::ret_code);
 
-  py::class_<PyTOPPRA>(m, "TOPPRA")
-      // .def(py::init<LinearConstraintPtrs, PyPiecewisePolyPath &>())
-      .def(py::init<py::list, PyPiecewisePolyPath &>())
-      .def("computePathParametrization", &PyTOPPRA::computePathParametrization,
+  py::class_<algorithm::TOPPRA>(m, "TOPPRA")
+      .def(py::init<LinearConstraintPtrs, const std::shared_ptr<PiecewisePolyPath> &>())
+      .def("computePathParametrization", &algorithm::TOPPRA::computePathParametrization,
            py::arg("vel_start") = 0, py::arg("vel_end") = 0)
-      .def("setN", &PyTOPPRA::setN)
-      .def_property_readonly("parametrizationData", &PyTOPPRA::getParameterizationData);
+      .def("setN", &algorithm::TOPPRA::setN)
+      .def_property_readonly("parametrizationData", &algorithm::TOPPRA::getParameterizationData);
 }
 }  // namespace python
 }  // namespace toppra
