b690592d138e4d51dd9890c7b88301f288d26dea
Duan Yutong <duan.yutong@dexai.com>
2020-12-17 04:40:59 +0000

    add main seciton

--- tests/dracula/test_zero_accel.py
+++ tests/dracula/test_zero_accel.py
@@ -1,7 +1,9 @@
 from scipy.interpolate import CubicSpline
+
 from toppra.dracula.zero_acceleration_start_end import impose_natural_bc
 
-t = [0, 3, 8, 12]
-knots = [(0, 0, 3, -2), (1, 2, 0, -3), (2, 0, 4, 2), (3, -1, 2, 0)]
-cspl = CubicSpline(t, knots, bc_type="clamped")
-impose_natural_bc(cspl)
+if __name__ == "__main__":
+    t = [0, 3, 8, 12]
+    knots = [(0, 0, 3, -2), (1, 2, 0, -3), (2, 0, 4, 2), (3, -1, 2, 0)]
+    cspl = CubicSpline(t, knots, bc_type="clamped")
+    impose_natural_bc(cspl)
