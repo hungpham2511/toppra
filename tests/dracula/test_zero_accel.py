43b48ddb85218681936dea99ec3be719eb55ad67
Duan Yutong <duan.yutong@dexai.com>
2020-12-17 04:36:31 +0000

    update import

--- tests/dracula/test_zero_accel.py
+++ tests/dracula/test_zero_accel.py
@@ -1,9 +1,7 @@
 from scipy.interpolate import CubicSpline
-from toppra.dracula.zero_acceleration_start_end import (
-    ZeroAccelerationAtStartAndEnd,
-)
+from toppra.dracula.zero_acceleration_start_end import impose_natural_bc
 
 t = [0, 3, 8, 12]
 knots = [(0, 0, 3, -2), (1, 2, 0, -3), (2, 0, 4, 2), (3, -1, 2, 0)]
 cspl = CubicSpline(t, knots, bc_type="clamped")
-ZeroAccelerationAtStartAndEnd(cspl)
+impose_natural_bc(cspl)
