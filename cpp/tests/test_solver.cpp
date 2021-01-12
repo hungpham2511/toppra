dc302f883c4d6067916fc786ea9c9b4ad0f8291a
Joseph Mirabel <jmirabel@laas.fr>
2021-01-12 09:51:54 +0100

    [C++] Simplify test_solver.cpp

--- cpp/tests/test_solver.cpp
+++ cpp/tests/test_solver.cpp
@@ -15,9 +15,11 @@
 
 constexpr int Ntrials = 10;
 
+std::map<std::string, toppra::Vectors> solutions;
+
 class Solver : public testing::Test {
 public:
-  Solver() : path(constructPath()) {
+  Solver() : path(constructPath()), g(Eigen::Vector2d{0.5, 2.}) {
 
     // path has the equation: 0 * x ^ 3 + 1 * x ^ 2 + 2 x ^ 1 + 3
   }
@@ -37,123 +39,65 @@ public:
   }
 
   std::shared_ptr<toppra::PiecewisePolyPath> path;
-};
+  toppra::Vector g;
 
-std::map<std::string, toppra::Vectors> solutions;
+  void testSolver(toppra::Solver& solver, const char* name)
+  {
+    using namespace toppra;
+
+    int nDof = path->dof();
+    Vector lb (-Vector::Ones(nDof)),
+           ub ( Vector::Ones(nDof));
+    LinearConstraintPtr ljv (new constraint::LinearJointVelocity (lb, ub));
+    LinearConstraintPtr lja (new constraint::LinearJointAcceleration (lb, ub));
+
+    int N = Ntrials;
+    Vector times (getTimes(N));
+
+    solver.initialize ({ ljv, lja }, path, times);
+
+    EXPECT_EQ(solver.nbStages(), N-1);
+    EXPECT_EQ(solver.nbVars(), 2);
+    ASSERT_EQ(solver.deltas().size(), N-1);
+    for (int i = 0; i < N-1; ++i)
+      EXPECT_NEAR(solver.deltas()[i], times[i+1] - times[i], 1e-10);
+
+    solver.setupSolver();
+    Vector solution;
+    Matrix H;
+    const value_type infty (std::numeric_limits<value_type>::infinity());
+    Bound x, xNext;
+    x << -infty, infty;
+    xNext << -infty, infty;
+    Vectors sols;
+    for (int i = 0; i < N; ++i) {
+      EXPECT_TRUE(solver.solveStagewiseOptim(i, H, g, x, xNext, solution));
+      EXPECT_EQ(solution.size(), solver.nbVars());
+      sols.emplace_back(solution);
+    }
+    solver.closeSolver();
+
+    solutions.emplace(name, sols);
+  }
+};
 
 #ifdef BUILD_WITH_qpOASES
 TEST_F(Solver, qpOASESWrapper) {
-  using namespace toppra;
-  int nDof = path->dof();
-  Vector lb (-Vector::Ones(nDof)),
-         ub ( Vector::Ones(nDof));
-  LinearConstraintPtr ljv (new constraint::LinearJointVelocity (lb, ub));
-  LinearConstraintPtr lja (new constraint::LinearJointAcceleration (lb, ub));
-
-  int N = Ntrials;
-  Vector times (getTimes(N));
-  solver::qpOASESWrapper solver;
-  solver.initialize ({ ljv, lja }, path, times);
-
-  EXPECT_EQ(solver.nbStages(), N-1);
-  EXPECT_EQ(solver.nbVars(), 2);
-  ASSERT_EQ(solver.deltas().size(), N-1);
-  for (int i = 0; i < N-1; ++i)
-    EXPECT_NEAR(solver.deltas()[i], times[i+1] - times[i], 1e-10);
-
-  solver.setupSolver();
-  Vector g (Vector::Ones(2)), solution;
-  Matrix H;
-  const value_type infty (std::numeric_limits<value_type>::infinity());
-  Bound x, xNext;
-  x << -infty, infty;
-  xNext << -infty, infty;
-  Vectors sols;
-  for (int i = 0; i < N; ++i) {
-    EXPECT_TRUE(solver.solveStagewiseOptim(i, H, g, x, xNext, solution));
-    EXPECT_EQ(solution.size(), solver.nbVars());
-    sols.emplace_back(solution);
-  }
-  solver.closeSolver();
-
-  solutions.emplace("qpOASES", sols);
+  toppra::solver::qpOASESWrapper solver;
+  testSolver(solver, "qpOASES");
 }
 #endif
 
 #ifdef BUILD_WITH_GLPK
 TEST_F(Solver, GLPKWrapper) {
-  using namespace toppra;
-  int nDof = path->dof();
-  Vector lb (-Vector::Ones(nDof)),
-         ub ( Vector::Ones(nDof));
-  LinearConstraintPtr ljv (new constraint::LinearJointVelocity (lb, ub));
-  LinearConstraintPtr lja (new constraint::LinearJointAcceleration (lb, ub));
-
-  int N = Ntrials;
-  Vector times (getTimes(N));
-  solver::GLPKWrapper solver;
-  solver.initialize ({ ljv, lja }, path, times);
-
-  EXPECT_EQ(solver.nbStages(), N-1);
-  EXPECT_EQ(solver.nbVars(), 2);
-  ASSERT_EQ(solver.deltas().size(), N-1);
-  for (int i = 0; i < N-1; ++i)
-    EXPECT_NEAR(solver.deltas()[i], times[i+1] - times[i], 1e-10);
-
-  solver.setupSolver();
-  Vector g (Vector::Ones(2)), solution;
-  Matrix H;
-  const value_type infty (std::numeric_limits<value_type>::infinity());
-  Bound x, xNext;
-  x << -infty, infty;
-  xNext << -infty, infty;
-  Vectors sols;
-  for (int i = 0; i < N; ++i) {
-    EXPECT_TRUE(solver.solveStagewiseOptim(i, H, g, x, xNext, solution));
-    EXPECT_EQ(solution.size(), solver.nbVars());
-    sols.emplace_back(solution);
-  }
-  solver.closeSolver();
-
-  solutions.emplace("GLPK", sols);
+  toppra::solver::GLPKWrapper solver;
+  testSolver(solver, "GLPK");
 }
 #endif
 
 TEST_F(Solver, Seidel) {
-  using namespace toppra;
-  int nDof = path->dof();
-  Vector lb (-Vector::Ones(nDof)),
-         ub ( Vector::Ones(nDof));
-  LinearConstraintPtr ljv (new constraint::LinearJointVelocity (lb, ub));
-  LinearConstraintPtr lja (new constraint::LinearJointAcceleration (lb, ub));
-
-  int N = Ntrials;
-  Vector times (getTimes(N));
-  solver::Seidel solver;
-  solver.initialize ({ ljv, lja }, path, times);
-
-  EXPECT_EQ(solver.nbStages(), N-1);
-  EXPECT_EQ(solver.nbVars(), 2);
-  ASSERT_EQ(solver.deltas().size(), N-1);
-  for (int i = 0; i < N-1; ++i)
-    EXPECT_NEAR(solver.deltas()[i], times[i+1] - times[i], 1e-10);
-
-  solver.setupSolver();
-  Vector g (Vector::Ones(2)), solution;
-  Matrix H;
-  const value_type infty (std::numeric_limits<value_type>::infinity());
-  Bound x, xNext;
-  x << -infty, infty;
-  xNext << -infty, infty;
-  Vectors sols;
-  for (int i = 0; i < N; ++i) {
-    EXPECT_TRUE(solver.solveStagewiseOptim(i, H, g, x, xNext, solution));
-    EXPECT_EQ(solution.size(), solver.nbVars());
-    sols.emplace_back(solution);
-  }
-  solver.closeSolver();
-
-  solutions.emplace("Seidel", sols);
+  toppra::solver::Seidel solver;
+  testSolver(solver, "Seidel");
 }
 
 // Check that all the solvers returns the same solution.
