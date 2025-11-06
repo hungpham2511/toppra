41aa070cb5d5cc170bac912a98aa87e7327789a4
Sebastian Castro <sebas.a.castro@gmail.com>
2025-11-06 01:02:05 -0500

    Update tasks

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-"""Collection of different operational tasts."""
+"""Collection of different operational tasks."""
 from invoke import task
 
 try:
@@ -7,7 +7,7 @@ except ImportError:
     import pathlib
 
 TOCHECK = [
-    "toppra",
+    "python/toppra",
 ]
 
 
@@ -103,11 +103,11 @@ def lint(c, pycodestyle=False, pydocstyle=False):
     """Run linting on selected source files."""
     c.run(
         "python -m pylint --rcfile=.pylintrc \
-                toppra/toppra/__init__.py \
-                toppra/toppra/utils.py \
-                toppra/toppra/interpolator.py \
-                toppra/toppra/exceptions.py \
-                toppra/toppra/parametrizer.py \
+                python/toppra/__init__.py \
+                python/toppra/utils.py \
+                python/toppra/interpolator.py \
+                python/toppra/exceptions.py \
+                python/toppra/parametrizer.py \
            "
     )
     # toppra/solverwrapper/solverwrapper.py
